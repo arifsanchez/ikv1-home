37a47d5d76869ddb7b15f5fa7bf5fc792e7d238f
paihz <mohdfaihz@gmail.com>
2013-05-16 14:28:56 +0800

    db new config

--- app/Config/database.php
+++ app/Config/database.php
@@ -24,6 +24,18 @@ class DATABASE_CONFIG {
 		'encoding' => 'utf8',
 	);
 	
+	
+		var $mt4 = array(
+			'datasource' => 'Database/Mysql',
+			'persistent' => false,
+			'host' => 'external-db.s138565.gridserver.com',
+			'login' => 'db138565_arif',
+			'password' => '>R1n"U8N{oP0',
+			'database' => 'db138565_mt4rs1',
+			'prefix' => '',
+			//'port' => '3306',
+		);
+	
 	public function __construct()
 	{
 	    if ($_SERVER['SERVER_NAME'] == 'iktrust.co.nz') {
